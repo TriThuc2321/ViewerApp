ad92fd2fa655885f29efe7f3d2fb49f9468d599f
TriThuc2321 <19522321@gm.uit.edu.vn>
2021-09-16 16:32:18 +0700

    a

---
+++ Viewer/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Viewer.Core;
+
+namespace Viewer.MVVM.ViewModel
+{
+    class HomeViewModel: ObservableObject
+    {
+        public HomeViewModel()
+        {
+
+        }
+    }
+}
---
+++ Viewer/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Viewer.Core;
+
+namespace Viewer.MVVM.ViewModel
+{
+    class MainViewModel: ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand AboutViewCommand { get; set; }
+        public RelayCommand DetailViewCommand { get; set; }
+
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public DetailViewModel DetailVM { get; set; }
+        public AboutViewModel AboutVM { get; set; }
+        
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DetailVM = new DetailViewModel();
+            AboutVM = new AboutViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => { CurrentView = HomeVM; });
+            AboutViewCommand = new RelayCommand(o => { CurrentView = AboutVM; });
+            DetailViewCommand = new RelayCommand(o => { CurrentView = DetailVM; });
+
+        }
+
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
